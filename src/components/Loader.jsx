443eea103307b70c446ec59520ccc20485067f9a
Calvin Lua <86276701+calvinlua@users.noreply.github.com>
2023-09-01 12:53:02 +0000

    Fixed all issues of 3D models  55:36 with reference : https://stackoverflow.com/questions/75655381/the-react-three-fiber-mesh-tag-is-unrecognized-in-this-browser

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+// import React from 'react'
+
+// const Loader = () => {
+//   return (
+//     <div>Loader</div>
+//   )
+// }
+
+// export default Loader
+
+
+import { Html, useProgress } from "@react-three/drei";
 
 const Loader = () => {
+  const { progress } = useProgress();
+
   return (
-    <div>Loader</div>
-  )
-}
+    <Html>
+      <span className="canvas-loader"></span>
+      <p>{progress.toFixed(2)}%</p>
+    </Html>
+  );
+};
 
 export default Loader
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import { Suspense, useEffect, useState} from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei'; //helper class 
+import { extend } from '@react-three/fiber'
 
+import CanvasLoader from '../Loader';
 const Computers = () => {
+
+  //import GLTF model
+  const computer = useGLTF('/public/desktop_pc/scene.gltf')
+
   return (
-    <div>Computers</div>
+    
+    <mesh>
+      {/* create light */}
+      <hemisphereLight intensity={0.15} groundColor="black"/>
+      <pointLight intensity={1}/>
+
+    {/* Call for computer object */}
+    <primitive 
+    object={computer.scene}
+    scale={0.75}
+    position={[0,-3.25, -1.5]} //x,y,z
+    rotation={[-0.01, -0.2, -0.1]}
+    />
+    </mesh>
   )
 }
 
-export default Computers
+
+//Draw the canvas to place the 3D model in
+const ComputersCanvas = () => {
+  return(
+
+   <Canvas 
+    frameloop="demand"
+    shadows
+    camera={{position: [20, 3, 5], fov: 25}} //x,y,z fov
+    gl={{preserveDrawingBuffer:true}}
+    >
+        
+        {/* React Loader */}
+        <Suspense fallback={<CanvasLoader />}>
+          <OrbitControls
+          enableZoom={false} 
+          maxPolarAngle={Math.PI/2} //enable left and right turn only >> angle
+          minPolarAngle={Math.PI/2}
+          />
+          <Computers/>
+       </Suspense>
+       <Preload all/> 
+   </Canvas>
+
+  )
+}
+
+
+
+export default ComputersCanvas
