4c708816ec1b4b0137da524343cda4a1af84ba12
Calvin Lua <wenzhenglua@gmail.com>
2023-09-17 07:01:04 +0800

    Update timeline component
    Fix timeline not showing issue. Return with () issue. Debugging by checking the card and console log or div text.

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,9 +1,80 @@
-import React from 'react'
+import React from "react";
+
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import { motion } from "framer-motion";
+import "react-vertical-timeline-component/style.min.css";
+
+import { styles } from "../styles";
+import { experiences } from "../constants";
+import SectionWrapper from "../hoc/SectionWrapper";
+import { textVariant } from "../utils/motion";
+
+//direct return with () without return statement
+const ExperienceCard = ({ index, experience, icon }) => (
+  //  card in timeline
+  <VerticalTimelineElement
+    contentStyle={{ background: "#1d1836", color: "#fff" }}
+    contentArrowStyle={{ borderRight: "7px solid #232631" }}
+    date={experience.date}
+    iconStyle={{ background: experience.iconBg }}
+    // timeline icons
+    icon={
+      <div className="flex justify-center items-center w-full h-full">
+        <img
+          src={experience.icon}
+          alt={experience.company_name}
+          className="w-[60%] h-[60%] object-contain"
+        />
+      </div>
+    }
+  >
+    <div>
+      <h3 className="text-white text-[24px] font-bold">{experience.title}</h3>
+      <p
+        className="text-secondary tex t-[16px] font-semibold"
+        style={{ margin: 0 }}
+      >
+        {experience.company_name}
+      </p>
+    </div>
+
+    <ul className="mt- list-disc ml-5 space-y-2">
+      {experience.points.map((point, index) => (
+        <li
+          key={`experience-point-${index}`}
+          className="text-white-100 text-[14px] pl-2 tracking-wider"
+        >
+          {point}
+        </li>
+      ))}
+    </ul>
+  </VerticalTimelineElement>
+);
 
 const Experience = () => {
   return (
-    <div>Experience</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>My Work Journey</p>
 
-export default Experience
+        <h2 className={styles.sectionHeadText}>Job Experience.</h2>
+      </motion.div>
+      <div className="mt-20 flex flex-col">
+        <VerticalTimeline>
+          {/* direct return */}
+          {experiences.map((experience, index) => (
+            <ExperienceCard
+              key={`experience-${index}`}
+              experience={experience}
+            ></ExperienceCard>
+          ))}
+        </VerticalTimeline>
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(Experience, "experience");
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,61 +1,62 @@
-import React from 'react'
-import {motion} from 'framer-motion'; //animation
-import {styles} from '../styles';
-import {ComputersCanvas} from './canvas';
-import { LoopRepeat } from 'three';
+import React from "react";
+import { motion } from "framer-motion"; //animation
+import { styles } from "../styles";
+import { ComputersCanvas } from "./canvas";
+import { LoopRepeat } from "three";
+import About from "./About";
 
 const Hero = () => {
   return (
     //fullscreen
-    <section className={'relative w-full h-screen mx-auto'}> 
-
-    <div className={`absolute 
-    inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}>
-    
+    <section className={"relative w-full h-screen mx-auto"}>
+      <div
+        className={`absolute 
+    inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+      >
         <div className="flex flex-col justify-center items-center mt-5">
-        {/* the rounded dot */}
-        <div className='w-5 h-5 rounded-full bg-[#915eff]'/>
-        {/* the gradient downward line */}
-        <div className='w-1 sm:h-80 h-40 violet-gradient'/>
+          {/* the rounded dot */}
+          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+          {/* the gradient downward line */}
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
         </div>
 
-      <div>
-        <h1 className={`${styles.heroHeadText} text-white`}> Hi I'm <span className='text-[#915eff]'>Calvin Lua</span>
-        </h1>
-        
-        <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-        As PLC programmer, IoT programmer, FullStack programmer and 3D Web Enthusiast
-        </p>
-        
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            {" "}
+            Hi I'm <span className="text-[#915eff]">Calvin Lua</span>
+          </h1>
+
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            As PLC programmer, IoT programmer, FullStack programmer and 3D Web
+            Enthusiast
+          </p>
+        </div>
       </div>
-    </div>
-        <ComputersCanvas/>
-        {/* on xs device bottom-10 but usually bottom-32 */}
-        <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
-          {/* anchor tag link */}
-          <a href='about'>
-              <div className='w-[35] h-[64px] rounded-3xl border-4 border-secondary
-              flex justify-center items-start p-2'>
-                <motion.div animate= {{
-                  y:[0,24,0] //motion down
-                }}
-                 transition={{
-                  duration:1.5,
-                  repeat:Infinity,
-                  repeatType: 'loop'
-
-                }}
-                className='w-3 h-3 rounded-full bg-secondary mb-1'
-
-
-                 />
-
-                
-              </div>
-          </a>
-        </div>
+      <ComputersCanvas />
+      {/* on xs device bottom-10 but usually bottom-32 */}
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        {/* anchor tag link */}
+        <a href="#about">
+          <div
+            className="w-[35] h-[64px] rounded-3xl border-4 border-secondary
+              flex justify-center items-start p-2"
+          >
+            <motion.div
+              animate={{
+                y: [0, 24, 0], //motion down
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
+        </a>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
