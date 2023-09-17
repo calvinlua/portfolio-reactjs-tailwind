bc39da5ea3efe00b678398af4e0d55986001a6af
Calvin Lua <wenzhenglua@gmail.com>
2023-09-17 10:06:20 +0800

    Create Tech ball
    Create technology page . Corrected Section Wrapper for tech page.

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import { React, Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import {
+  Decal,
+  Float,
+  OrbitControls,
+  Preload,
+  useTexture,
+} from "@react-three/drei";
+import CanvasLoader from "../Loader";
 
-const Ball = () => {
+const Ball = (props) => {
+  // destructure decal, useTexture (url)
+  const [decal] = useTexture([props.imgUrl]);
   return (
-    <div>Ball</div>
-  )
-}
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+      <ambientLight intensity={0.25} />
+      <directionalLight position={[0, 0, 0.05]} />
+      {/* determine the scale of the size of the ball */}
+      <mesh castShadow receiveShadow scale={2.75}>
+        <icosahedronGeometry args={[1, 1]} />
+      </mesh>
+    </Float>
+  );
+};
 
-export default Ball
+const BallCanvas = ({ icon }) => {
+  return (
+    <Canvas frameloop="demand" gl={{ preserveDrawingBuffer: true }}>
+      {/* React Loader */}
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls enablePan={false} enableZoom={false} />
+        {/* call for ball object */}
+        <Ball imgUrl={icon} />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default BallCanvas;
