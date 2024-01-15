551c37925f99f4906e5f2fe61348e2cc2add2f04
Calvin Lua <wenzhenglua@gmail.com>
2024-01-15 21:19:39 +0800

    Test CI/CD pipeline from Github Action to Vercel. Update one line for Full Stack Engineer Description.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -35,7 +35,7 @@ const ServiceCard = ({ index, title, icon }) => {
             <img src={icon} alt="title" className="w-16 h-16 object-contain" />
             <h3
               className="text-white text-[20px]
-        font-bold text-center"
+        font-bold text-center "
             >
               {title}
             </h3>
